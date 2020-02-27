01a5fc301364b5e15feab4803875a0c7c6303c73
Selvaraj Chinnasamy <selvaraj.chinnasamy@softwaysolutions.com>
2020-02-27 16:36:33 +0530

    vedio

--- vedio-store.js
+++ vedio-store.js
@@ -1,17 +1,27 @@
 const PiCamera = require('pi-camera');
-const myCamera = new PiCamera({
-    mode: 'video',
-    output: `${__dirname}/video.h264`,
-    width: 1920,
-    height: 1080,
-    timeout: 5000,
-    nopreview: true,
-});
 
-myCamera.record()
-    .then((result) => {
-        console.log('vedio was captured');
-    })
-    .catch((error) => {
-        console.log('error', error);
+function SeriesOfVedio() {
+    const dateTime = new Date();
+    const vedio = captureVedio(dateTime);
+    vedio.record()
+        .then((result) => {
+            console.log('vedio was captured');
+            SeriesOfVedio();
+        })
+        .catch((error) => {
+            console.log('error', error);
+        });
+}
+
+function captureVedio(name) {
+    return new PiCamera({
+        mode: 'video',
+        output: `${__dirname}/${name}.h264`,
+        width: 1920,
+        height: 1080,
+        timeout: 5000,
+        nopreview: true,
     });
+}
+
+SeriesOfVedio();
